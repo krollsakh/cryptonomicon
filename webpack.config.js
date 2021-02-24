7e602ea9681585b6d28c12fa117545bf141baf44
Sergey Romanov <romanov@bcstech.ru>
2021-02-24 09:43:45 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+// webpack config
+const path = require('path');
+const WebpackBar = require('webpackbar');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const { VueLoaderPlugin } = require('vue-loader');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const resolve = (dir) => {
+  return path.resolve(__dirname, dir);
+};
+
+module.exports = (env = {}) => ({
+  context: resolve('src/'),
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    app: './app.ts'
+  },
+  output: {
+    path: resolve("dist"),
+    filename: "[name].[hash:5].bundle.js",
+    publicPath: process.env.BASE_URL,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: { babelrc: true }
+          },
+          {
+            loader: "ts-loader",
+            options: { appendTsSuffixTo: [/\.vue$/] }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      'vue': '@vue/runtime-dom',
+      '@': resolve('src/'),
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new WebpackBar(),
+    new VueLoaderPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  // devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'src/public'),
+    publicPath: process.env.BASE_URL,
+    index: './index.html',
+    hot: true,
+    stats: 'minimal',
+    quiet: true,
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    historyApiFallback: true,
+  }
+});
